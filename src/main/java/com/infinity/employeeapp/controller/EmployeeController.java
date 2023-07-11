66d0d724aa916520b8740b1d904924e91a3dcb71
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-07-12 01:22:54 +0530

    [Added]:[Vikas] Home API implement in Controler Layer

---
+++ src/main/java/com/infinity/employeeapp/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.infinity.employeeapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employeeapp")
+public class EmployeeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to Employee App";
+	}
+}
